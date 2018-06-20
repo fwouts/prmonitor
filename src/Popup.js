c5847970a579c5021945c8d0f69e5c8d472855f6
Francois Wouts <f@zenc.io>
2018-06-20 14:53:46 +1000

    Set up a React-based Chrome extension

---
+++ src/Popup.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Popup.css";
+
+class Popup extends Component {
+  render() {
+    return (
+      <div className="Popup">
+        <p>Hello, I'm a popup.</p>
+      </div>
+    );
+  }
+}
+
+export default Popup;
---
+++ src/background.js
@@ -0,0 +1,1 @@
+// This is the background script of the Chrome extension.

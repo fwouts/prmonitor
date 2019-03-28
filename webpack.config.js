e95c08262e52b5128b0f10eb075e784043c32d8d
Francois Wouts <f@zenc.io>
2019-03-29 08:00:49 +1100

    Remove unnecessary chunk splitting (irrelevant for a Chrome extension)

--- webpack.config.js
+++ webpack.config.js
@@ -74,18 +74,6 @@ module.exports = {
     ])
   ],
   optimization: {
-    minimizer: [new TerserPlugin()],
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/
-        }
-      },
-      chunks: "async",
-      minChunks: 1,
-      minSize: 30000,
-      name: true
-    }
+    minimizer: [new TerserPlugin()]
   }
 };

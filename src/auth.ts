87deedebc550b6d68dc33bf0fa159b69ae41f059
Francois Wouts <f@zenc.io>
2019-03-29 21:56:53 +1100

    Remove usage of chrome.storage.sync
    We stopped writing to it months ago, every user would have updated already.

--- src/auth.ts
+++ src/auth.ts
@@ -2,37 +2,8 @@
  * Returns the GitHub API token stored in settings.
  */
 export async function getGitHubApiToken(): Promise<string> {
-  try {
-    const localToken = await getGitHubApiTokenFromLocalStorage();
-    return localToken;
-  } catch {
-    // Older versions of PR Monitor saved the token in sync storage.
-    const syncToken = await getGitHubApiTokenFromSyncStorage();
-    return syncToken;
-  }
-}
-
-/**
- * Fetches the GitHub API token from local storage.
- */
-function getGitHubApiTokenFromLocalStorage() {
   return new Promise<string>((resolve, reject) => {
     chrome.storage.local.get(["gitHubApiToken"], result => {
-      if (result.gitHubApiToken) {
-        resolve(result.gitHubApiToken);
-      } else {
-        reject(new Error("GitHub API token is not set."));
-      }
-    });
-  });
-}
-
-/**
- * Gets GitHub API token from sync storage instead of local storage.
- */
-function getGitHubApiTokenFromSyncStorage() {
-  return new Promise<string>((resolve, reject) => {
-    chrome.storage.sync.get(["gitHubApiToken"], result => {
       if (result.gitHubApiToken) {
         resolve(result.gitHubApiToken);
       } else {
@@ -53,9 +24,5 @@ export function updateGitHubApiToken(token: string) {
       },
       resolve
     );
-    // Erase from sync storage if it was stored previously.
-    chrome.storage.sync.set({
-      gitHubApiToken: null
-    });
   });
 }

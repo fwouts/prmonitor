7ce1d451f4d5b9f7d6e89c18efb86171f090de7c
Francois Wouts <f@zenc.io>
2019-03-31 17:03:17 +1100

    Split out PullRequestList and Summary components

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,38 +1,13 @@
-import styled from "@emotion/styled";
 import { observer } from "mobx-react";
 import React, { Component } from "react";
 import { GitHubState } from "../state/github";
-import { Header } from "./design/Header";
+import { PullRequestList } from "./PullRequestList";
 import { Settings } from "./Settings";
+import { Summary } from "./Summary";
 
 export interface PopupProps {
   github: GitHubState;
 }
-
-const Error = styled.p`
-  border: 1px solid #d00;
-  background: #fdd;
-  color: #400;
-  padding: 8px;
-`;
-
-const PullRequestList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const PullRequest = styled.li`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  border: 1px solid #ddd;
-  padding: 8px;
-  margin: 8px 0;
-`;
-
-const PullRequestLink = styled.a`
-  display: block;
-  text-decoration: none;
-  color: #333;
-`;
 
 @observer
 class Popup extends Component<PopupProps> {
@@ -43,74 +18,10 @@ class Popup extends Component<PopupProps> {
   render() {
     return (
       <div>
-        {this.renderLoading()}
-        {this.renderUserLogin()}
-        {this.renderRepoList()}
-        {this.renderPullRequestsSection()}
+        <Summary github={this.props.github} />
+        <PullRequestList github={this.props.github} />
         <Settings github={this.props.github} />
       </div>
-    );
-  }
-
-  renderLoading() {
-    if (this.props.github.status === "loaded") {
-      return <></>;
-    }
-    return <div>Please wait...</div>;
-  }
-
-  renderUserLogin() {
-    if (!this.props.github.user) {
-      return <></>;
-    }
-    return (
-      <div>
-        Signed in as <b>{this.props.github.user.login}</b>
-      </div>
-    );
-  }
-
-  renderRepoList() {
-    if (!this.props.github.repoList) {
-      return <></>;
-    }
-    return (
-      <div>You have access to {this.props.github.repoList.length} repos</div>
-    );
-  }
-
-  renderPullRequestsSection() {
-    if (this.props.github.lastError) {
-      return <Error>Error: {this.props.github.lastError}</Error>;
-    }
-    return (
-      <div>
-        <Header>Incoming pull requests</Header>
-        {this.renderPullRequestList()}
-      </div>
-    );
-  }
-
-  renderPullRequestList() {
-    if (!this.props.github.token) {
-      return <p>Please provide an API token below.</p>;
-    }
-    if (this.props.github.unreviewedPullRequests === null) {
-      return <p>Loading pull requests...</p>;
-    }
-    if (this.props.github.unreviewedPullRequests.length === 0) {
-      return <p>Nothing to review, yay!</p>;
-    }
-    return (
-      <PullRequestList>
-        {this.props.github.unreviewedPullRequests.map(pullRequest => (
-          <PullRequest>
-            <PullRequestLink target="_blank" href={pullRequest.html_url}>
-              {pullRequest.title}
-            </PullRequestLink>
-          </PullRequest>
-        ))}
-      </PullRequestList>
     );
   }
 }

bcc1c6dd9b0c161573cb64f2f3a5172f5b467cc4
Francois Wouts <francois.wouts@airtasker.com>
2019-04-08 12:00:14 +1000

    Add debug statements to ensure background checks work as expected

--- src/background/entry.ts
+++ src/background/entry.ts
@@ -3,32 +3,34 @@ import { checkPullRequests } from "./check-pull-requests";
 import { onNotificationClicked } from "./notifications";
 
 // This is the entry point of the background script of the Chrome extension.
-
-const CHECK_PULL_REQUESTS_ALARM_KEY = "check-pull-requests";
+console.debug("Background entry point running...");
 
 // Beause it isn't a persistent background script, we cannot simply use
 // setInterval() to schedule regular checks for new pull requests.
 // Instead, we set an alarm three minutes.
 // IMPORTANT: GitHub API only allows us 50 requests per hour in total.
-chromeApi.alarms.create(CHECK_PULL_REQUESTS_ALARM_KEY, {
+chromeApi.alarms.create({
   periodInMinutes: 3
 });
 
 // When alarm is triggered, call checkPullRequests().
 chromeApi.alarms.onAlarm.addListener(alarm => {
-  if (alarm.name === CHECK_PULL_REQUESTS_ALARM_KEY) {
-    triggerRefresh();
-  }
+  console.debug("Alarm triggered", alarm);
+  triggerRefresh();
 });
 
 chromeApi.runtime.onMessage.addListener(message => {
+  console.debug("Message received", message);
   if (message.kind === "refresh") {
     triggerRefresh();
   }
 });
 
 // Also call checkPullRequests() on install.
-chromeApi.runtime.onInstalled.addListener(triggerRefresh);
+chromeApi.runtime.onInstalled.addListener(() => {
+  console.debug("Extension installed");
+  triggerRefresh();
+});
 
 chromeApi.notifications.onClicked.addListener(onNotificationClicked);
 

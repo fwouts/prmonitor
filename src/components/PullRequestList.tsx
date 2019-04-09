3293f33d30082fa54c750e4558af3d70eca2a514
François Wouts <f@zenc.io>
2019-04-09 18:48:44 +1000

    Show the avatar of the author on each PR (#79)

--- src/components/PullRequestList.tsx
+++ src/components/PullRequestList.tsx
@@ -11,7 +11,8 @@ export interface PullRequestListProps {
 
 const PullRequest = styled.a`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
   text-decoration: none;
   border-bottom: 1px solid #eee;
 
@@ -24,31 +25,52 @@ const PullRequest = styled.a`
   }
 `;
 
-const Title = styled.div`
-  padding: 8px;
-  color: #000;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-const Information = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 4px 8px;
-  font-size: 0.9em;
-  color: #555;
+const Title = styled.div`
+  color: #000;
+  padding: 8px;
 `;
 
 const Repo = styled.div`
-  width: 200px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  font-size: 0.9em;
+  color: #555;
+  padding: 8px;
 `;
 
-const Author = styled.div`
+const AuthorWidth = "80px";
+const AuthorAvatarSize = "40px";
+
+const AuthorBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${AuthorWidth};
+  padding: 8px;
+`;
+
+const AuthorAvatar = styled.img`
+  width: ${AuthorAvatarSize};
+  height: ${AuthorAvatarSize};
+  border: 2px solid #333;
+  border-radius: 50%;
+`;
+
+const AuthorLogin = styled.div`
   white-space: nowrap;
-  overflow: hidden;
   text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 0.9em;
+  color: #555;
+  max-width: ${AuthorWidth};
 `;
 
 @observer
@@ -64,14 +86,19 @@ export class PullRequestList extends Component<PullRequestListProps> {
             target="_blank"
             href={pullRequest.htmlUrl}
           >
-            <Title>{pullRequest.title}</Title>
-            <Information>
+            <Info>
+              <Title>{pullRequest.title}</Title>
               <Repo>
                 {pullRequest.repoOwner}/{pullRequest.repoName} (#
                 {pullRequest.pullRequestNumber})
               </Repo>
-              <Author>{pullRequest.authorLogin}</Author>
-            </Information>
+            </Info>
+            <AuthorBox title={pullRequest.authorLogin}>
+              {pullRequest.author && (
+                <AuthorAvatar src={pullRequest.author.avatarUrl} />
+              )}
+              <AuthorLogin>{pullRequest.authorLogin}</AuthorLogin>
+            </AuthorBox>
           </PullRequest>
         ))}
       </List>

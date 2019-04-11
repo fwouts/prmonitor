c68261ac416b5df648489d77d9633e6ba0a21c32
Francois Wouts <f@zenc.io>
2019-04-11 17:15:43 +1000

    Fix broken test and clarify

--- src/state/filtering/repos-pushed-after.spec.ts
+++ src/state/filtering/repos-pushed-after.spec.ts
@@ -1,16 +1,17 @@
-import { repoPushedAfterPredicate } from "./repos-pushed-after";
+import { repoWasPushedAfter } from "./repos-pushed-after";
 
 describe("repoWasPushedAfter", () => {
   it("always returns true when pushedAfter = null", () => {
+    const predicate = repoWasPushedAfter(null);
     expect(
-      repoPushedAfterPredicate(null)({
+      predicate({
         owner: "test",
         name: "test",
         pushedAt: "2012-03-15T17:00:11Z"
       })
     ).toBe(true);
     expect(
-      repoPushedAfterPredicate(null)({
+      predicate({
         owner: "test",
         name: "test",
         pushedAt: "2052-03-15T17:00:11Z"
@@ -19,16 +20,16 @@ describe("repoWasPushedAfter", () => {
   });
 
   it("filters correctly", () => {
-    const filter = repoPushedAfterPredicate("2012-03-15T17:00:11Z");
+    const predicate = repoWasPushedAfter("2012-03-15T17:00:11Z");
     expect(
-      filter({
+      predicate({
         owner: "test",
         name: "test",
         pushedAt: "2010-03-15T17:00:11Z"
       })
     ).toBe(false);
     expect(
-      filter({
+      predicate({
         owner: "test",
         name: "test",
         pushedAt: "2020-03-15T17:00:11Z"

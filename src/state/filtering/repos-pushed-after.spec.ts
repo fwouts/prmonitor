92d35e5b2f9acb6729fb8a8441d891a12f48b00a
Francois Wouts <f@zenc.io>
2019-04-11 17:14:33 +1000

    Add a test for repoWasPushedAfter

---
+++ src/state/filtering/repos-pushed-after.spec.ts
@@ -0,0 +1,38 @@
+import { repoPushedAfterPredicate } from "./repos-pushed-after";
+
+describe("repoWasPushedAfter", () => {
+  it("always returns true when pushedAfter = null", () => {
+    expect(
+      repoPushedAfterPredicate(null)({
+        owner: "test",
+        name: "test",
+        pushedAt: "2012-03-15T17:00:11Z"
+      })
+    ).toBe(true);
+    expect(
+      repoPushedAfterPredicate(null)({
+        owner: "test",
+        name: "test",
+        pushedAt: "2052-03-15T17:00:11Z"
+      })
+    ).toBe(true);
+  });
+
+  it("filters correctly", () => {
+    const filter = repoPushedAfterPredicate("2012-03-15T17:00:11Z");
+    expect(
+      filter({
+        owner: "test",
+        name: "test",
+        pushedAt: "2010-03-15T17:00:11Z"
+      })
+    ).toBe(false);
+    expect(
+      filter({
+        owner: "test",
+        name: "test",
+        pushedAt: "2020-03-15T17:00:11Z"
+      })
+    ).toBe(true);
+  });
+});

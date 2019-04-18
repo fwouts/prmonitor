978774cf29ebd366c3435bbcfe75b1c5df9c6616
Francois Wouts <f@zenc.io>
2019-04-18 20:37:31 +1000

    Add a bit of JSDoc

--- src/github-api/api.ts
+++ src/github-api/api.ts
@@ -7,20 +7,41 @@ import {
 } from "@octokit/rest";
 import { ReviewState } from "../storage/loaded-state";
 
+/**
+ * A simple wrapper around GitHub's API.
+ */
 export interface GitHubApi {
+  /**
+   * Returns the information about the current authenticated user.
+   */
   loadAuthenticatedUser(): Promise<GetAuthenticatedUserResponse>;
 
+  /**
+   * Returns the full list of repositories for the user.
+   */
   loadRepos(): Promise<ReposListResponse>;
 
+  /**
+   * Returns the full list of pull requests in a given state for a repository.
+   */
   loadPullRequests(
     repo: RepoReference,
     state: "open" | "closed" | "all"
   ): Promise<PullsListResponse>;
 
+  /**
+   * Returns a single pull request.
+   */
   loadSinglePullRequest(pr: PullRequestReference): Promise<PullsGetResponse>;
 
+  /**
+   * Returns the full list of reviews for a pull request.
+   */
   loadReviews(pr: PullRequestReference): Promise<PullsListReviewsResponse>;
 
+  /**
+   * Returns the full list of comments for a pull request.
+   */
   loadComments(pr: PullRequestReference): Promise<IssuesListCommentsResponse>;
 }
 

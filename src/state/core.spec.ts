ec8d8607ef78ed53c510c9b52179ba0198f1b994
Francois Wouts <f@zenc.io>
2019-04-18 15:12:47 +1000

    Add a bunch of tests

--- src/state/core.spec.ts
+++ src/state/core.spec.ts
@@ -1,20 +1,142 @@
 import { buildTestingEnvironment } from "../environment/testing/fake";
+import {
+  MuteConfiguration,
+  NOTHING_MUTED
+} from "../storage/mute-configuration";
 import { Core } from "./core";
 
 describe("Core", () => {
-  it("loads without token", async () => {
+  it("loads correctly without token", async () => {
     const env = buildTestingEnvironment();
     const core = new Core(env);
+
+    // No token stored.
     env.store.token.load.mockReturnValue(Promise.resolve(null));
+
+    // Other things are stored, they should be ignored.
+    env.store.lastError.load.mockReturnValue(Promise.resolve("error"));
+    env.store.lastCheck.load.mockReturnValue(
+      Promise.resolve({
+        userLogin: "fwouts",
+        repos: [],
+        openPullRequests: []
+      })
+    );
+    env.store.notifiedPullRequests.load.mockReturnValue(
+      Promise.resolve(["a", "b", "c"])
+    );
+    env.store.muteConfiguration.load.mockReturnValue(
+      Promise.resolve({
+        mutedPullRequests: [
+          {
+            repo: {
+              owner: "zenclabs",
+              name: "prmonitor"
+            },
+            number: 1,
+            until: {
+              kind: "next-update",
+              mutedAtTimestamp: 123
+            }
+          }
+        ]
+      })
+    );
+
+    // Initialise.
     await core.load();
+
+    expect(core.token).toBeNull();
+    expect(core.lastError).toBeNull();
+    expect(core.refreshing).toBe(false);
+    expect(core.loadedState).toBeNull();
+    expect(core.muteConfiguration).toEqual(NOTHING_MUTED);
+    expect(core.notifiedPullRequestUrls.size).toBe(0);
   });
 
   it("loads with token", async () => {
     const env = buildTestingEnvironment();
     const core = new Core(env);
-    env.store.token.load.mockReturnValue(Promise.resolve("abc"));
+
+    // No token stored.
+    env.store.token.load.mockReturnValue(Promise.resolve("valid-token"));
+
+    // Other things are stored, they should be restored.
+    const state = {
+      userLogin: "fwouts",
+      repos: [],
+      openPullRequests: []
+    };
+    const notifiedPullRequestUrls = ["a", "b", "c"];
+    const muteConfiguration: MuteConfiguration = {
+      mutedPullRequests: [
+        {
+          repo: {
+            owner: "zenclabs",
+            name: "prmonitor"
+          },
+          number: 1,
+          until: {
+            kind: "next-update",
+            mutedAtTimestamp: 123
+          }
+        }
+      ]
+    };
+    env.store.lastError.load.mockReturnValue(Promise.resolve("error"));
+    env.store.lastCheck.load.mockReturnValue(Promise.resolve(state));
+    env.store.notifiedPullRequests.load.mockReturnValue(
+      Promise.resolve(notifiedPullRequestUrls)
+    );
+    env.store.muteConfiguration.load.mockReturnValue(
+      Promise.resolve(muteConfiguration)
+    );
+
+    // Initialise.
     await core.load();
+
+    expect(core.token).toEqual("valid-token");
+    expect(core.lastError).toEqual("error");
+    expect(core.refreshing).toBe(false);
+    expect(core.loadedState).toEqual(state);
+    expect(core.muteConfiguration).toEqual(muteConfiguration);
+    expect(Array.from(core.notifiedPullRequestUrls)).toEqual(
+      notifiedPullRequestUrls
+    );
   });
+
+  it("reloads when receiving a load message", () => {
+    const env = buildTestingEnvironment();
+
+    // Initialize the core.
+    // TODO: Consider adding the listener in a separate init() method.
+    new Core(env);
+    env.messenger.trigger({
+      kind: "reload"
+    });
+    expect(env.store.token.load).toHaveBeenCalled();
+  });
+
+  it("doesn't reload on non-reload message", () => {
+    const env = buildTestingEnvironment();
+
+    // Initialize the core.
+    // TODO: Consider adding the listener in a separate init() method.
+    new Core(env);
+    env.messenger.trigger({
+      kind: "refresh"
+    });
+    expect(env.store.token.load).not.toHaveBeenCalled();
+  });
+
+  it.todo("resets state and triggers a refresh when a new token is set");
+  it.todo("doesn't refresh when not authenticated");
+  it.todo("doesn't refresh when offline");
+  it.todo("updates badge when it starts refreshing");
+  it.todo("updates badge and error when it finishes refreshing successfully");
+  it.todo("updates badge and error when it fails to refresh");
+  it.todo("notifies of new pull requests and saves notified state");
+  it.todo("updates badge after muting a PR");
 
   it("doesn't duplicate muted pull requests", async () => {
     const env = buildTestingEnvironment();

c7478830fa0630d47127b30487f68dbc15b89678
Francois Wouts <f@zenc.io>
2019-04-19 15:44:59 +1000

    Fix up the background for the empty list

--- src/components/PullRequestList.tsx
+++ src/components/PullRequestList.tsx
@@ -1,9 +1,16 @@
+import styled from "@emotion/styled";
 import { observer } from "mobx-react";
 import React, { Component } from "react";
 import { PullRequest } from "../storage/loaded-state";
-import { List } from "./design/List";
 import { Paragraph } from "./design/Paragraph";
 import { PullRequestItem } from "./PullRequestItem";
+
+export const List = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 0 8px 8px 8px;
+  background: #fff;
+  margin-bottom: 16px;
+`;
 
 export interface PullRequestListProps {
   pullRequests: PullRequest[];
@@ -16,19 +23,21 @@ export interface PullRequestListProps {
 @observer
 export class PullRequestList extends Component<PullRequestListProps> {
   render() {
-    return this.props.pullRequests.length === 0 ? (
-      <Paragraph>{this.props.emptyMessage}</Paragraph>
-    ) : (
+    return (
       <List>
-        {this.props.pullRequests.map(pullRequest => (
-          <PullRequestItem
-            key={pullRequest.nodeId}
-            pullRequest={pullRequest}
-            allowMuting={this.props.allowMuting}
-            onOpen={this.props.onOpen}
-            onMute={this.props.onMute}
-          />
-        ))}
+        {this.props.pullRequests.length === 0 ? (
+          <Paragraph>{this.props.emptyMessage}</Paragraph>
+        ) : (
+          this.props.pullRequests.map(pullRequest => (
+            <PullRequestItem
+              key={pullRequest.nodeId}
+              pullRequest={pullRequest}
+              allowMuting={this.props.allowMuting}
+              onOpen={this.props.onOpen}
+              onMute={this.props.onMute}
+            />
+          ))
+        )}
       </List>
     );
   }
--- src/components/design/List.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled";
-
-export const List = styled.div`
-  border: 1px solid #ddd;
-  border-radius: 0 8px 8px 8px;
-  background: #fff;
-`;

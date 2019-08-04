604b114a7c056f5b810c88f6fa668aa899bbda68
Francois Wouts <f@zenc.io>
2019-08-04 22:25:47 +1000

    Sort PRs by most recent first

--- src/loading/implementation.ts
+++ src/loading/implementation.ts
@@ -1,5 +1,5 @@
 import { buildGitHubApi } from "../github-api/implementation";
-import { LoadedState } from "../storage/loaded-state";
+import { LoadedState, PullRequest } from "../storage/loaded-state";
 import { GitHubLoader } from "./api";
 import { refreshOpenPullRequests } from "./internal/pull-requests";
 
@@ -11,8 +11,22 @@ async function load(token: string): Promise<LoadedState> {
   const githubApi = buildGitHubApi(token);
   const user = await githubApi.loadAuthenticatedUser();
   const openPullRequests = await refreshOpenPullRequests(githubApi, user.login);
+  const sorted = [...openPullRequests].sort((a, b) => {
+    return getPullRequestTimestamp(b) - getPullRequestTimestamp(a);
+  });
   return {
     userLogin: user.login,
-    openPullRequests
+    openPullRequests: sorted
   };
+}
+
+function getPullRequestTimestamp(pr: PullRequest) {
+  let prTimestamp = new Date(pr.updatedAt).getTime();
+  for (const comment of pr.comments) {
+    prTimestamp = Math.max(prTimestamp, new Date(comment.createdAt).getTime());
+  }
+  for (const review of pr.reviews) {
+    prTimestamp = Math.max(prTimestamp, new Date(review.submittedAt).getTime());
+  }
+  return prTimestamp;
 }

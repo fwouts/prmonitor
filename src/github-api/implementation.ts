bb568eb76b428afb28036510c655517992e1805a
Toliver Jue <teejae@gmail.com>
2021-07-26 14:20:44 +0900

    node

--- src/github-api/implementation.ts
+++ src/github-api/implementation.ts
@@ -122,7 +122,7 @@ export function buildGitHubApi(token: string): GitHubApi {
         const result = response.repository.pullRequest;
         const reviewDecision = result.reviewDecision;
         const checkStatus =
-          result.commits.nodes[0].commit.statusCheckRollup?.state;
+          result.commits.nodes?.[0]?.commit.statusCheckRollup?.state;
         return {
           reviewDecision,
           checkStatus,

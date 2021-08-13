9d52bdb6c5661216fbf72173757ae9493f6188e1
Toliver <teejae@gmail.com>
2021-08-13 19:33:15 +0900

    Popup: Fix second monitor refreshing of text boxes on Macs (#878)

--- src/popup.tsx
+++ src/popup.tsx
@@ -29,31 +29,56 @@ const env = buildEnvironment(chromeApiSingleton);
 const core = new Core(env);
 core.load().catch(console.error);
 
+let globalCssTemplates = `
+  @import url("https://fonts.googleapis.com/css?family=Roboto");
+
+  body {
+    background: #f6f8fc;
+    color: #444;
+    margin: 0 auto;
+    padding: 8px;
+    width: 600px;
+    font-family: Roboto, sans-serif;
+    font-size: 14px;
+  }
+
+  a {
+    color: #000;
+  }
+
+  .nav-tabs {
+    border-bottom: none;
+  }
+`;
+
+// Temporary workaround for secondary monitors on MacOS where redraws don't happen
+// TODO: Remove once Chromium fixes bug.
+// Ref: https://stackoverflow.com/questions/56500742/why-is-my-google-chrome-extensions-popup-ui-laggy-on-external-monitors-but-not/64113061#64113061
+// @See https://bugs.chromium.org/p/chromium/issues/detail?id=971701
+const isSecondMonitor =
+  window.screenLeft < 0 ||
+  window.screenTop < 0 ||
+  window.screenLeft > window.screen.width ||
+  window.screenTop > window.screen.height;
+if (isSecondMonitor) {
+  globalCssTemplates += `
+    @keyframes redraw {
+      0% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0.99;
+      }
+    }
+    html {
+      animation: redraw 1s linear infinite;
+    }
+  `;
+}
+
 ReactDOM.render(
   <>
-    <Global
-      styles={css`
-        @import url("https://fonts.googleapis.com/css?family=Roboto");
-
-        body {
-          background: #f6f8fc;
-          color: #444;
-          margin: 0 auto;
-          padding: 8px;
-          width: 600px;
-          font-family: Roboto, sans-serif;
-          font-size: 14px;
-        }
-
-        a {
-          color: #000;
-        }
-
-        .nav-tabs {
-          border-bottom: none;
-        }
-      `}
-    />
+    <Global styles={css(globalCssTemplates)} />
     <Popup core={core} />
   </>,
   document.getElementById("root")

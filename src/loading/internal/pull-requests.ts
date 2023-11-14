ee37fa459c801038082890d0ada8ebdd7bad69cd
dbharris2 <dbharris2@gmail.com>
2023-11-14 13:44:41 -0700

    Removed approved PRs from needs review

--- src/loading/internal/pull-requests.ts
+++ src/loading/internal/pull-requests.ts
@@ -24,7 +24,7 @@ export async function refreshOpenPullRequests(githubApi: GitHubApi): Promise<Pul
   // Note: each query should specifically exclude the previous ones so we don't end up having
   // to deduplicate PRs across lists.
   const reviewRequestedPullRequests = await githubApi.searchPullRequests(
-    `review-requested:@me -author:@me is:open`
+    `review-requested:@me -author:@me is:open -review:approved`
   );
   const needsRevisionPullRequests = await githubApi.searchPullRequests(
     `is:open -author:@me review:changes_requested involves:@me`
